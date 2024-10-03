0511010ed0cce7db3bb7761e9dfbd4e07f2b12a8
Milagros Ernst <milagrosunamfceqyn@gmail.com>
2024-10-03 20:10:45 -0300

    modificacion numero

--- Taller-Git/Form1.cs
+++ Taller-Git/Form1.cs
@@ -15,6 +15,8 @@ namespace Taller_Git
         private void button2_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hola Mundo 2");
+            int numero = 1;
+            MessageBox.Show($"El numero es:{numero}");
         }
     }
 }
